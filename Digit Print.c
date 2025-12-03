c156d86023e681c87cef08bc31e568e0b81bbf54
MD. RAZAUL KARIM <mdrazaulkarim698@gmail.com>
2025-12-03 21:42:49 +0600

    Add files via upload

---
+++ Digit Print.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int number[]={10,20,30,40,50,60,70,80,90,100};
+
+	for(int index=0;index<100;index++)
+    {
+        printf("%d\n", index);
+    }
+
+return 0;
+}
